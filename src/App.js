d2113ff12f5572cb7cde4a2b6a929e9bad79128a
geshan99 <geshansudasinghe@gmail.com>
2022-04-08 21:30:02 +0530

    Step component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FeesSection from "./Components/FeesSection/FeesSection";
 import Banner from "./Components/Banner/Banner";
 import Form from "./Components/Form/Form";
 import DivorceWitnessSection from "./Components/DivorceWitnessSection/DivorceWitnessSection";
+import HowtoUse from "./Components/Form/HowtoUse/HowtoUse";
 
 function App() {
 	return (
@@ -10,6 +11,9 @@ function App() {
 			<FeesSection />
 			<Banner></Banner>
 			<DivorceWitnessSection></DivorceWitnessSection>
+			<Banner></Banner>
+			<HowtoUse></HowtoUse>
+			<Banner></Banner>
 			<Form></Form>
 		</div>
 	);
---
+++ src/Components/Form/HowtoUse/StepBox.js
@@ -0,0 +1,64 @@
+import DownArrowIcon from "../../../assets/down-arrow.svg";
+
+function StepBox({ title, content, footer, number, type, footer2, isLast }) {
+	return type === "1" ? (
+		<div className="step-box">
+			<div className="step-circle">
+				<h4>STEP</h4>
+				<h3>{number}</h3>
+			</div>
+			<h1 className="step-heading">{title}</h1>
+			<p className="step-content">
+				<span>
+					<a
+						href="/"
+						style={{ marginRight: "5px", color: "#3594AA" }}
+					>
+						Privacy
+					</a>
+				</span>
+				{content}
+			</p>
+			<p className="step-footer">{footer}</p>
+			<p className="step-footer">{footer2}</p>
+			<div className="arrow">
+				<img
+					src={DownArrowIcon}
+					alt="down-arrow"
+					width={"64px"}
+					style={{ marginTop: "16px" }}
+				></img>
+			</div>
+		</div>
+	) : isLast ? (
+		<div className="step-box">
+			<div className="step-circle">
+				<h4>STEP</h4>
+				<h3>{number}</h3>
+			</div>
+			<h1 className="step-heading">{title}</h1>
+			<p className="step-content">{content}</p>
+			<p className="step-footer">{footer}</p>
+		</div>
+	) : (
+		<div className="step-box">
+			<div className="step-circle">
+				<h4>STEP</h4>
+				<h3>{number}</h3>
+			</div>
+			<h1 className="step-heading">{title}</h1>
+			<p className="step-content">{content}</p>
+			<p className="step-footer">{footer}</p>
+			<div className="arrow">
+				<img
+					src={DownArrowIcon}
+					alt="down-arrow"
+					width={"64px"}
+					style={{ marginTop: "16px" }}
+				></img>
+			</div>
+		</div>
+	);
+}
+
+export default StepBox;
