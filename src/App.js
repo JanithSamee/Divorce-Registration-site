8c578eebaff84b35a7ac791ee2882f58a3b9b869
geshan99 <geshansudasinghe@gmail.com>
2022-04-08 16:52:03 +0530

    form added to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Components/Banner/Banner";
+import Form from "./Components/Form/Form";
 
 function App() {
 	return (
 		<div>
 			<Banner></Banner>
+			<Form></Form>
 		</div>
 	);
 }
