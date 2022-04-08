f4f60c7e4272ce3c27773c801d1631c9a3f3fe17
GimhanRanasinghe <gimhanravi@gmail.com>
2022-04-08 22:53:53 +0530

    added card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-import Banner from "./Components/Banner/Banner";
+import MyCard from "./Components/MyCard/MyCard";
+import icon from "../src/assets/icon1.svg";
+
+
 
 function App() {
-	return (
-		<div>
-			<Banner></Banner>
-		</div>
-	);
+  return (
+    <div>
+      <MyCard
+        number="01"
+        title="対面不要"
+        body="メールと書面の郵送のみで手続きが完結するため、対面でのやり取りは不要です。そのためお忙しい方やプライバシーを気にする方も安心してご利用できます。"
+        imagePath={icon}
+      ></MyCard>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/MyCard/MyCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./myCard-styles.css";
+
+
+function MyCard({ number, title, body, imagePath }) {
+  
+  return (
+    <>
+      <div className="card-container">
+        <div className="number">{number}</div>
+        <div className="band">{title}</div>
+        <div className="card-body">{body}</div>
+        <div className="image">
+          <img src={imagePath} alt="image" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MyCard;
