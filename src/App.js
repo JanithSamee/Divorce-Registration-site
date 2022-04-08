b05c2dee261cfd1529576cc300bb068d8f3de3a2
GimhanRanasinghe <gimhanravi@gmail.com>
2022-04-09 02:01:59 +0530

    card set

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-import MyCard from "./Components/MyCard/MyCard";
-import icon from "../src/assets/icon1.svg";
+import MyCardSection from "./Components/MyCard/MyCardSection";
 
 
 
 function App() {
   return (
     <div>
-      <MyCard
-        number="01"
-        title="対面不要"
-        body="メールと書面の郵送のみで手続きが完結するため、対面でのやり取りは不要です。そのためお忙しい方やプライバシーを気にする方も安心してご利用できます。"
-        imagePath={icon}
-      ></MyCard>
+      <MyCardSection></MyCardSection>
     </div>
   );
 }
