4a76651e29c7c1c405df94c662495eee3cd70fd0
Tharindu Gimras <tharindugimras@gmail.com>
2022-04-09 11:58:03 +0530

    Finish second page and add fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import FirstPage from './Pages/FirstPage';
+import SecondPage from './Pages/SecondPage';
+import './fonts/Hiragino Mincho ProN W6.otf';
+import './fonts/Hiragino Kaku Gothic ProN W3.otf';
 
 function App() {
   return (
     <div className="App">
       <FirstPage />
+      <SecondPage />
     </div>
   );
 }
---
+++ src/Pages/SecondPage.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import './SecondPage.css';
+import secondPageBottomImg from '../Assets/グループ 12.svg';
+
+function SecondPage() {
+  return (
+    <Container fluid={true} className="secondPageContainer">
+      <Row
+        className="secondPageContainer-title font-ultra"
+        style={{ fontWeight: 600, letterSpacing: '0.5vw' }}
+      >
+        家族や知人に離婚したことを
+        <br /> 知られたくない
+      </Row>
+      <Row
+        className="secondPageContainer-middlecontent"
+        style={{ fontWeight: 100 }}
+      >
+        こんな想いはありませんか？
+        <br />
+        <br />
+        <br />
+        離婚届の証人については家族や知人に頼むのが一般的です。 <br />
+        <div>
+          しかし、中には
+          <span>証人が見つからない方や家族や知人に離婚したことを知ら</span>
+        </div>
+        <div>
+          <span>れたくない</span>という方もいらっしゃると思います。
+        </div>
+        <br />
+        <br />
+        <br />
+        全く知らない赤の他人に依頼することも可能ですが、個人情報の取り扱い
+        <br />
+        などの問題が発生します。
+      </Row>
+      <Row>
+        <img src={secondPageBottomImg} alt="" />
+      </Row>
+    </Container>
+  );
+}
+
+export default SecondPage;
