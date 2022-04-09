cc6a83a0e669f3f58a6508f3179a152dfa3aa8db
Tharindu Gimras <tharindugimras@gmail.com>
2022-04-09 13:28:31 +0530

    Fix drop shadow for small screens

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
-import FirstPage from "./Components/FirstSection/FirstPage";
-import SecondPage from "./Components/SecondSection/SecondPage";
-import "./fonts/Hiragino Mincho ProN W6.otf";
-import "./fonts/Hiragino Kaku Gothic ProN W3.otf";
-import Footer from "./Components/Footer/Footer";
+import './App.css';
+import FirstPage from './Components/FirstSection/FirstPage';
+import SecondPage from './Components/SecondSection/SecondPage';
+import './fonts/Hiragino Mincho ProN W6.otf';
+import './fonts/Hiragino Kaku Gothic ProN W3.otf';
+import Footer from './Components/Footer/Footer';
 
 function App() {
-	return (
-		<div className="App">
-			<FirstPage />
-			<SecondPage />
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <FirstPage />
+      <SecondPage />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
